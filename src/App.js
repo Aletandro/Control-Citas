83d6842c5283cae50aece34e337b1c2b41f25a28
Alejandro M <1993.lamg@gmail.com>
2022-10-26 17:52:34 -0500

    Proyecto terminado

--- src/App.js
+++ src/App.js
@@ -1,28 +1,57 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Formulario from "./components/Formulario";
+import Cita from "./components/Cita";
 
 function App() {
+  //citas en local storage
+
+  let citasIniciales = JSON.parse(localStorage.getItem("citas"));
+  if (!citasIniciales) {
+    citasIniciales = [];
+  }
+
   //Arreglo citas
   const [citas, setCitas] = useState([]);
+
+  useEffect(() => {
+    if (citasIniciales) {
+      localStorage.setItem("citas", JSON.stringify(citas));
+    } else {
+      localStorage.setItem("citas", JSON.stringify([]));
+    }
+  }, [citas, citasIniciales]);
 
   // Func Citas Act + nuevas
 
   const crearCita = (cita) => {
-    setCitas({
-      ...citas,
-      cita,
-    });
+    setCitas([...citas, cita]);
   };
+
+  //eliminar cita por ident
+
+  const eliminarCita = (id) => {
+    const nuevasCitas = citas.filter((cita) => cita.id !== id);
+    setCitas(nuevasCitas);
+  };
+
+  //mensaje condicional
+  const titulo = citas.length === 0 ? "No Hay citas" : "Administrador de citas";
 
   return (
     <>
-      <h1>Administrador de Pascientes</h1>
+      <h1>Administrador de Pacientes</h1>
+
       <div className="container">
         <div className="row">
           <div className="one-half column">
             <Formulario crearCita={crearCita} />
           </div>
-          <div className="one-half column"></div>
+          <div className="one-half column">
+            <h2>{titulo}</h2>
+            {citas.map((cita) => (
+              <Cita key={cita.id} cita={cita} eliminarCita={eliminarCita} />
+            ))}
+          </div>
         </div>
       </div>
     </>
--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { v4 as uuidv4 } from "uuid";
+import PropTypes from "prop-types";
 
 const Formulario = ({ crearCita }) => {
   //Crear State
